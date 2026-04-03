b7466a36c739bdc4e564f70be1367a73e7037d41
Romain Claudot <romain_claudot@hotmail.com>
2026-04-03 12:37:52 +0200

    pundit set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include Pundit::Authorization
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  # On skip pundit quand il s'agit d'admin controllers (pas encore utilisés ici)
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 
   protected
 
@@ -11,6 +19,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     profile_user_path(resource)
   end
+
+  # Les permitted parameters doivent être précisés, ce sont les champs en plus rajoutés à
+  # l'inscription, on les précise pour le sign-up et la màj du compte.
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,22 @@
 class UsersController < ApplicationController
 
   def show
-    
+  @user = current_user
+  authorize @user
   end
 
   def edit
-    
+  @user = current_user
+  authorize @user
   end
 
   def update
-    
+  @user = current_user
+  authorize @user
   end
 
   def destroy
-    
+  @user = current_user
+  authorize @user
   end
 end
